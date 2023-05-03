7c95a7555bfd4e34e128ba6f07a11a22ae83907a
Arthur Lin <arslin@ucsc.edu>
2023-05-02 21:55:49 -0700

    Mod named, Name added to project, Sources cited.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// Arthur Lin Spring 2023
+// Rocket Patrol Faded
+// Project took ~ 6-8 hours
+// Music: https://www.youtube.com/watch?v=60ItHLz5WEA
 let config = {
     type: Phaser.AUTO,
     width: 640,
