e0c49d95d563f72a3f025b99cc58ebbb28a2f8e4
Arthur Lin <arslin@ucsc.edu>
2023-05-06 19:23:30 -0700

    Explanatory comment fixed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 // Arthur Lin Spring 2023
 // Rocket Patrol Faded
 // Project took ~ 6-8 hours
+// Mods Chosen:
+// New Spaceship (15)
+// Alternating two player mode (15)
+// New Timing mechanism (15)
+// Particle  Emitter (15)
+
+// New Title screen (10)
+// Time Remaining in Seconds (10)
+
+// Music in background (5)
+// Speed increase (5)
+// New scrolling tile sprite (5)
+// Allow the player to control the rocket after it is fired (5)
+
+
 // Music: https://www.youtube.com/watch?v=60ItHLz5WEA
 let config = {
     type: Phaser.AUTO,
